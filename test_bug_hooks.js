c512afb59d609f2e14ba29758e3e5c648ecbc12b
darkship <crumble_tk@hotmail.com>
2015-04-22 20:24:12 +0200

    allows for item2

--- test_bug_hooks.js
+++ test_bug_hooks.js
@@ -1,6 +1,8 @@
-item= new Mongo.Collection("items")
-item2= new Mongo.Collection("items2")
-var value= item2.insert({title: "title1"})
+item = new Mongo.Collection("items")
+item2 = new Mongo.Collection("items2")
+var value = item2.insert({
+  title: "title1"
+})
 
 
 item.attachSchema(new SimpleSchema({
@@ -9,58 +11,76 @@ item.attachSchema(new SimpleSchema({
   }
 }))
 
-item.before.insert(function(userId, doc){
+item.before.insert(function(userId, doc) {
   console.log('function before insert')
 })
-item.after.insert(function(userId, doc){
+item.after.insert(function(userId, doc) {
   console.log('function after insert')
 })
-item.before.remove(function(userId, doc){
+item.before.remove(function(userId, doc) {
   console.log('function before remove')
 })
-item.after.remove(function(userId, doc){
+item.after.remove(function(userId, doc) {
   console.log('function after remove')
 })
 
-item.before.update(function(userId, doc, fieldNames, modifier, options){
+item.before.update(function(userId, doc, fieldNames, modifier, options) {
   console.log('function before update')
-  item2.update(value, {$set: {
-    title: "titlevalue"
-  }})
+  item2.update(value, {
+    $set: {
+      title: "titlevalue"
+    }
+  }, function(err) {
+    if (err)
+      console.error(err)
+  })
 })
 
-item.after.update(function(userId, doc, fieldNames, modifier, options){
+item.after.update(function(userId, doc, fieldNames, modifier, options) {
   console.log('function after update')
 })
 
 if (Meteor.isClient) {
   Meteor.subscribe("item")
-  var id= item.insert({
+  var id = item.insert({
     title: "title"
   })
 
   item.update({
     _id: id
-  }, {$set:{
-    title: "new title"
-  }})
+  }, {
+    $set: {
+      title: "new title"
+    }
+  })
   item.remove(id)
 }
 
 if (Meteor.isServer) {
-  Meteor.publish("item", function(){
+  Meteor.publish("item", function() {
     item.find({})
   })
 
   item.allow({
-    insert: function(){
-        return true
-        },
-    update: function(){
-        return true
+    insert: function() {
+      return true
     },
-    remove: function(){
-        return true
+    update: function() {
+      return true
+    },
+    remove: function() {
+      return true
+    }
+  })
+  item2.allow({
+    insert: function() {
+      return true
+    },
+    update: function() {
+      return true
+    },
+    remove: function() {
+      return true
     }
   })
 }
